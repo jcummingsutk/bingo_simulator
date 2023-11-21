e1148f885a165216080832c8fbdc1b704f145c32
John Cummings <jcummingsutk@gmail.com>
2023-11-21 18:58:49 -0500

    typo, renaming, card generator

--- bingo_simulator/bingo_card/test/test_bingo_card.py
+++ bingo_simulator/bingo_card/test/test_bingo_card.py
@@ -3,7 +3,7 @@ import pytest
 from bingo_simulator.board import BingoCard
 
 
-def test_ilegal_board():
+def test_illegal_board():
     bad_board = [[1, 2], [2]]
     with pytest.raises(ValueError):
         BingoCard(bad_board)
--- bingo_simulator/board/board_generator.py
+++
@@ -1,6 +0,0 @@
-from typing import Protocol, Union
-
-
-class RandomNumberGenerator(Protocol):
-    def generate(self) -> Union[float, int]:
-        """"""
