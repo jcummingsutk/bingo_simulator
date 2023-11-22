4847692de3fb5753a6344328926259ff906afdff
John Cummings <jcummingsutk@gmail.com>
2023-11-22 12:39:36 -0500

    Bingo Card Generator

--- bingo_simulator/bingo_card/test/test_bingo_card.py
+++ bingo_simulator/bingo_card/test/test_bingo_card.py
@@ -1,9 +1,22 @@
 import pytest
 
 from bingo_simulator.bingo_card.bingo_card import BingoCard
+from bingo_simulator.bingo_card.bingo_card_generator import generate_bingo_card
+from bingo_simulator.random_number_generator import SampleWithoutReplacement
 
 
 def test_illegal_board():
     bad_board = [[1, 2], [2]]
     with pytest.raises(ValueError):
         BingoCard(bad_board)
+
+
+def test_board_generator():
+    number_generator = SampleWithoutReplacement(list(range(75)))
+    expected_card_values = [
+        [number_generator.sample_list[i] for i in range(3)],
+        [number_generator.sample_list[i] for i in range(3, 6)],
+        [number_generator.sample_list[i] for i in range(6, 9)],
+    ]
+    bingo_card = generate_bingo_card(3, 3, number_generator)
+    assert expected_card_values == bingo_card.values
--- bingo_simulator/bingo_card/test/test_card_generator.py
+++ bingo_simulator/bingo_card/test/test_card_generator.py
@@ -1,7 +1,7 @@
 from bingo_simulator.bingo_card.bingo_card_generator import generate_bingo_card
 
 
-class IncrementingNumberGenerator:
+class IncrementingNumberGeneratorTest:
     def __init__(self, start_value: int):
         self.value = start_value
 
@@ -12,7 +12,7 @@ class IncrementingNumberGenerator:
 
 
 def test_card_generator():
-    number_generator = IncrementingNumberGenerator(3)
+    number_generator = IncrementingNumberGeneratorTest(3)
     nrows = 2
     ncols = 4
     bingo_card = generate_bingo_card(
@@ -20,6 +20,6 @@ def test_card_generator():
     )
     assert bingo_card.nrows == nrows
     assert bingo_card.ncols == ncols
+
     expected_result = [[3, 4, 5, 6], [7, 8, 9, 10]]
-    for expected_row, result_row in zip(expected_result, bingo_card.values):
-        assert expected_row == result_row
+    assert expected_result == bingo_card.values
