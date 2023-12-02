a15c9b8840234d35cf8e0fc890706961170b77d6
John Cummings <jcummingsutk@gmail.com>
2023-12-02 11:38:42 -0500

    root path for fast api

--- bingo_simulator/app/main.py
+++ bingo_simulator/app/main.py
@@ -8,7 +8,7 @@ from bingo_simulator.bingo_card.bingo_card_generator import generate_bingo_card
 from bingo_simulator.bingo_game import BingoGame
 from bingo_simulator.random_number_generator import SampleWithoutReplacement
 
-app = FastAPI(root_path="/beta/")
+app = FastAPI()
 
 handler = Mangum(app)
 
