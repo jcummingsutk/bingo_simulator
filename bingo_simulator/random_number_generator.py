1aea0f3ea1e2a9ffe28eb16565c2b560e07ac88b
John Cummings <jcummingsutk@gmail.com>
2023-12-04 13:12:24 -0500

    docs

--- bingo_simulator/random_number_generator.py
+++ bingo_simulator/random_number_generator.py
@@ -2,9 +2,23 @@ import random
 
 
 class SampleWithoutReplacement:
+    """Generator that samples from a list without replacement by shuffling up a list
+    and, on calling the generate function, retrieves the first item of the list
+    """
+
     def __init__(self, sample_list: list[int]):
+        """
+        Args:
+            sample_list (list[int]): the list of numbers that should be drawn from
+        """
         self.sample_list = sample_list
         random.shuffle(self.sample_list)
 
     def generate(self) -> int:
+        """Returns the first item of the shuffled list to simulate a random number
+        bing drawn
+
+        Returns:
+            int: number from the list
+        """
         return self.sample_list.pop(0)
